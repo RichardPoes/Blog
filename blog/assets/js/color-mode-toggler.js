bb117be1ff882b93ccd99f8ae8c097da5600b3dc
RichardPoes <richard.poes@outlook.com>
2024-10-07 23:08:22 +0200

    Create a token based design system

--- blog/assets/js/color-mode-toggler.js
+++ blog/assets/js/color-mode-toggler.js
@@ -31,9 +31,9 @@
       }
   
       const themeSwitcherText = document.querySelector('#bd-theme-text')
-      const activeThemeIcon = document.querySelector('.theme-icon-active use')
+      const activeThemeIcon = document.querySelector('.theme-icon-active i')
       const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-      const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+      const spanOfActiveBtn = btnToActive.querySelector('span i').getAttribute('class')
   
       document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
         element.classList.remove('active')
@@ -42,7 +42,7 @@
   
       btnToActive.classList.add('active')
       btnToActive.setAttribute('aria-pressed', 'true')
-      activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+      activeThemeIcon.setAttribute('class', spanOfActiveBtn)
       const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
       themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
   
